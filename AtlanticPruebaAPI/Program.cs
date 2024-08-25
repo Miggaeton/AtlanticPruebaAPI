ff7ea33ca530e4804e83be662aec211595cf33d6
Miguel Bulla Duque <miggaeton0926@gmail.com>
2024-08-25 09:39:14 -0500

    API actualizada

--- AtlanticPruebaAPI/Program.cs
+++ AtlanticPruebaAPI/Program.cs
@@ -4,7 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins(
+                "http://localhost:4200"
+                )
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,11 +27,12 @@ builder.Services.AddDbContext<InsuredContext>(options =>
 
 var app = builder.Build();
 
+/*
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<InsuredContext>();
     context.Database.Migrate();
-}
+} */
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,6 +40,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors();
 
 app.UseHttpsRedirection();
 
